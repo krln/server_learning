00e827e50d19849d05ed29b0974536ed42c93462
krln <krlnkmsk88@gmail.com>
2015-01-19 11:17:56 +0100

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'server_learning'
+set :repo_url, 'git@github.com:krln/server_learning.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+#set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  task :start do
+    execute :rails, "server ", "--daemon"
+  end
+
+  task :stop do
+    execute :kill, "-TERM $(cat tmp/pids/server.pid)"
+  end
+  desc "Restart the application"
+  task :restart do
+    invoke "deploy:stop"
+    invoke "deploy:start"
+  end
+end
